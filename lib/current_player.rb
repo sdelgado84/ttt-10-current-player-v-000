c32fcb87c0595a22402763636d27ba8290ce9b24
Salvador Delgado <sdelgado84@gmail.com>
2018-03-02 16:48:03 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(turn)
+  #counter = 1
+  move_count = []
+  turn.each do |unit|
+    #counter = 0
+    if unit == "X" || unit == "O"
+      #puts "spot taken"
+      move_count.push(1)
+    #else 
+      #puts "spot free"
+      #counter += 1
+    end 
+  end 
+    
